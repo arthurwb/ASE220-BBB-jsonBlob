d41e6e90671ca05a3ffc1c24706c8f02be2d8914
Brooks Arthur <brooksarthur07@gmail.com>
2023-03-22 16:09:11 -0400

    header and footer

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ var http = require('http');
 var fs=require('fs');
 const url=require('url');
 
+const httpRequest = {
+    GET: function(URLpath, res) {
+        if(fs.existsSync('.' + URLpath.pathname + '.json')){
+            jsonFile = fs.readFileSync(`.${URLpath.pathname}.json`).toString()
+            res.write(`
+                <textarea rows = "20" cols = "60" style='width: 600px'>${jsonFile}</textarea>
+            `);
+            console.log('xxxxx');
+        }else{
+            res.write("test");
+        }
+    },
+    PUT: function() {
+
+    },
+    POST: function() {
+
+    },
+    DELETE: function() {
+
+    }
+}
+
 //create a server object:
 http.createServer(function (req, res) {
     let url_components=url.parse(req.url,true);
 
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    if(fs.existsSync('.' + url_components.pathname + '.json')){
-        jsonFile = fs.readFileSync(`.${url_components.pathname}.json`).toString()
-		res.write(`
-            <input type="textarea" rows = "5" cols = "60" value='${jsonFile}' style='width: 600px'>
-        `);
-		console.log('xxxxx');
-	}else{
-		res.write("test");
-	}
+    let header = fs.readFileSync("view/header.html");
+    let footer = fs.readFileSync("view/footer.html");
+
+    res.write(header);
+    
+    httpRequest.GET(url_components, res);
+
+    res.write(footer);
 
     res.end();
 }).listen(8080); //the server object listens on port 8080
